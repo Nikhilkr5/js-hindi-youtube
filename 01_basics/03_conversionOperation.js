3f45bdbc47efe04736aa51ae93d2181d44c0dfc6
Nikhilkr5 <nikhilkumar108thakur@gmail.com>
2025-07-05 09:03:36 +0000

    ConversionOperation

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,152 @@
+// let score = "33"
+// console.log(typeof score);
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber)
+
+
+
+
+// let score = "33abc"
+// console.log(typeof score);
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);    //NaN 
+
+
+
+
+// let score = null
+// console.log(typeof score);
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);    //NaN 
+
+
+
+// let score = undefined
+// console.log(typeof score);
+// console.log(typeof(score))
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber)
+// console.log(valueInNumber);    //NaN 
+
+//similarly you can check for let score= true , "nikhil"
+
+
+
+/*
+//note: conversion: 
+"33"  ==> 33
+"33abc" ==> NaN
+true ==> 1 ; false==> 0
+*/
+
+/*
+ let isLoggedIn = 1
+
+ let booleanIsLoggedIn = Boolean(isLoggedIn)
+ console.log(booleanIsLoggedIn);
+*/
+
+ 
+
+// let isLoggedIn = ""
+ 
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+/*
+let isLoggedIn = "Nikhil"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+*/
+
+
+
+/*
+// note:
+1 ==> true ; 0 ==> false
+"" ==> false
+"Nikhil"  ==> true
+*/
+
+
+
+// let someNumber = 33
+
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+
+// ************ OPERATIONS*********
+/*
+let value = 3
+let negValue = -value
+console.log(negValue)
+*/
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2/2);
+// console.log(2%3);
+
+
+/*
+let str1 = "hello"
+let str2= " hitesh"
+
+let str3 = str1 + str2
+console.log(str3)
+*/
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+"2")
+// console.log("1" + 2 + 2)
+// console.log(1+ 2 + "2");
+// console.log(1+ 2 + 2);
+
+
+// console.log((3 + 4) * 5 % 3);
+
+
+console.log(true)
+console.log(+true)
+//console.log(true+)  // error
+
+
+
+
+let num1, num2 , num3
+
+num1 = num2 = num3 = 2 + 2  // not a goood way
+
+
+let gameCounter = 100
+// gameCounter++;                  //101
+++gameCounter;                      //101
+console.log(gameCounter);
+
+
+
+
+
+// study prefix and postfix operator    from js mdn(y searching prefix and postfix js mdn)
+
